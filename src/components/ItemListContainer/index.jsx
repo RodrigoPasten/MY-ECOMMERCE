e31a986829040ceb86bab318c4afaf66eafb0f5b
Rodrigo Pasten <rodrigo.pasten.c@gmail.com>
2022-07-30 16:40:14 -0400

    contador con botòn

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,8 +1,20 @@
+import ItemCount from "../ItemCount";
 import React from "react";
 import Title from '../Title';
-export const ItemListContainer = () => {
+
+
+export const ItemListContainer = ({text}) => {
+
+    const onAdd = (quantity) => {
+        console.log(`Haz agregado ${quantity} unidades`);
+    }
+
+
     return(
-        <Title greeting='Rodrigo Pasten' />
+        <>
+            <Title greeting={text} />
+            <ItemCount initial ={1} stock={10} onAdd={onAdd} />
+        </>
     );
 }
 
